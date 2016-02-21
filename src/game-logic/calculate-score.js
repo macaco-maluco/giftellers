7152e822941c2315cb0371a87e4615cdd9e01680
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-02-21 23:43:52 +0100

    preventing players to go outside the score board ⛔️

--- src/game-logic/calculate-score.js
+++ src/game-logic/calculate-score.js
@@ -1,3 +1,5 @@
+const MAX_SCORE = 21
+
 export default function calculateScore (game) {
   const { shuffledVotingCards, players } = game
   const playersArray = Object.keys(players).map(id => players[id])
@@ -10,10 +12,12 @@ export default function calculateScore (game) {
       .filter(player => !player.isStoryTeller && player.votedCardIndex === index)
 
     if (card === storyTeller.selectedCard) {
-      voters.forEach(voter => voter.score += 3)
+      voters.forEach(voter => voter.score < MAX_SCORE ? voter.score += 3 : null)
 
       // not everyone guessed correctly
-      if (voters.length !== playersArray.length - 1 && voters.length > 0) {
+      if (voters.length !== playersArray.length - 1 &&
+          voters.length > 0 &&
+          storyTeller.score < MAX_SCORE) {
         storyTeller.score += 3
       }
     } else {
@@ -21,7 +25,7 @@ export default function calculateScore (game) {
 
       voters
         .filter(voter => voter !== cardOwner)
-        .forEach(voter => cardOwner.score += 1)
+        .forEach(voter => cardOwner.score < MAX_SCORE ? cardOwner.score += 1 : null)
     }
   })
 
